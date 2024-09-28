497af0c1da07c1ee6776cb85b03c4d96d4572624
mwawrzy2 <53042410+kindaCacti@users.noreply.github.com>
2024-09-28 19:16:02 +0200

    added random problem

--- main_system/Problem.py
+++ main_system/Problem.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Problem:
     def __init__(self, _name, _link, _rating, _problem_id, _is_solved = False, _time_begin = 0, _time_end = 0, _tags = None):
         self.name = _name
@@ -31,6 +33,14 @@ class Problem:
     
     def get_problem_id(self):
         return self.problem_id
+    
+    def stringify(self):
+        output_string = ""
+        output_string += str(self.get_name()) + " "
+        output_string += str(self.get_problem_id()) + " "
+        output_string += str(self.get_rating()) + " "
+        output_string += str(self.get_tags()) + " "
+        return output_string
 
 def calculate_performence(problem):
     performence = problem.getRating() * 10
@@ -41,6 +51,7 @@ def calculate_performence(problem):
 def evaluate_problems(problems):
     performence = 0
     ammount_of_problems = len(problems)
+
     for problem in problems:
         performence += calculate_performence(problem)
     
@@ -49,12 +60,14 @@ def evaluate_problems(problems):
 
 def get_tags_from_problems(problems):
     all_tags = []
+
     for problem in problems:
         problem_tags = problem.get_tags()
         for tag in problem_tags:
             if tag in all_tags:
                 continue
             all_tags.append(tag)
+
     return all_tags
 
 def sort_problems_by_difficulty(problems):
@@ -76,3 +89,38 @@ def sort_problems_by_tags(problems):
         sorted_problems[tag] = sort_problems_by_difficulty(sorted_problems[tag])
     
     return sorted_problems
+
+def filter_problems_by_tag(problems, tag):
+    filtered_problems = []
+
+    for problem in problems:
+        problem_tags = problem.get_tags()
+        if tag in problem_tags:
+            filtered_problems.append(problem)
+    
+    return filtered_problems
+
+def filter_problems_by_rating(problems, min_rating, max_rating):
+    filtered_problems = []
+
+    for problem in problems:
+        if problem.get_rating() < min_rating:
+            continue
+        if problem.get_rating() > max_rating:
+            continue
+
+        filtered_problems.append(problem)
+    
+    return filtered_problems
+
+
+def get_random_problem_with_tag(problems, tag):
+    filtered_problems = filter_problems_by_tag(problems, tag)
+
+    ammount_of_problems = len(filtered_problems)
+
+    if ammount_of_problems == 0:
+        return None
+    
+    random_number = randint(0, ammount_of_problems-1)
+    return filtered_problems[random_number]
